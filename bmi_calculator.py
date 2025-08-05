c47e4f398d79038e990f5dc82e6cb12a58319838
NISHACHANDRA1302 <nisarganc28@gmail.com>
2025-08-05 17:20:49 +0530

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Fy1uvrTro3Wf50v1Svb5x81X9cj4RaND
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import os
